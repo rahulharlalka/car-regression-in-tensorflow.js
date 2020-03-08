1fa4e8c16b1b9458d41ad6513b304e47f7e3d625
rahul harlalka <rahulharlalka96@gmail.com>
2020-03-08 15:10:06 +0530

    my first commit:

---
+++ script.js
@@ -0,0 +1,151 @@
+console.log("hello tensorflow")
+
+async function getData() {
+    const carsDataReq = await fetch('https://storage.googleapis.com/tfjs-tutorials/carsData.json');  
+    const carsData = await carsDataReq.json();  
+    const cleaned = carsData.map(car => ({
+      mpg: car.Miles_per_Gallon,
+      horsepower: car.Horsepower,
+    }))
+    .filter(car => (car.mpg != null && car.horsepower != null));
+    
+    return cleaned;
+  }
+
+ function createmodel(){
+
+    const model=tf.sequential();
+    
+    model.add(tf.layers.dense({inputShape:[1],units:20,useBais:true,activation:'relu'}));
+    model.add(tf.layers.dense({units:1,useBais:true,activation:'relu'}));   
+
+    return model;
+}
+
+function convertToTensor(data){
+    return tf.tidy(()=>{
+        tf.util.shuffle(data);
+
+        const inputs = data.map(d=>d.horsepower)
+        const labels =data.map(d=>d.mpg);
+
+        const inputTensor = tf.tensor2d(inputs,[inputs.length,1]);
+        const labelTensor = tf.tensor2d(labels,[labels.length,1]);
+        
+        const inputMax = inputTensor.max();
+        const inputMin = inputTensor.min();  
+        const labelMax = labelTensor.max();
+        const labelMin = labelTensor.min();
+
+        const normalizedInputs = inputTensor.sub(inputMin).div(inputMax.sub(inputMin));
+        const normalizedLabels = labelTensor.sub(labelMin).div(labelMax.sub(labelMin));
+
+        return {
+            inputs:normalizedInputs,
+            labels:normalizedLabels,
+
+            inputMax,
+            inputMin,
+            labelMax,
+            labelMin,
+        }
+
+
+    });
+}
+
+async function trainmodel(model,inputs,labels){
+    model.compile({
+        optimizer:tf.train.adam(),
+        loss:tf.losses.meanSquaredError,
+        metrics:['mse'],
+    });
+
+    const batchsize =32;
+    const epochs=50;
+
+    return await model.fit(inputs,labels,{
+        batchsize,
+        epochs,
+        shuffle:true,
+        callbacks:tfvis.show.fitCallbacks(
+            {name:'training performance'},
+            ['loss','mse'],
+            {'height':200,callbacks:['onEpochEnd']}
+        )
+    });
+}
+
+
+function testModel(model, inputData, normalizationData) {
+    const {inputMax, inputMin, labelMin, labelMax} = normalizationData;  
+ 
+    const [xs, preds] = tf.tidy(() => {
+      
+      const xs = tf.linspace(0, 1, 100);      
+      const preds = model.predict(xs.reshape([100, 1]));      
+      
+      const unNormXs = xs
+        .mul(inputMax.sub(inputMin))
+        .add(inputMin);
+      
+      const unNormPreds = preds
+        .mul(labelMax.sub(labelMin))
+        .add(labelMin);
+
+      return [unNormXs.dataSync(), unNormPreds.dataSync()];
+    });
+    
+   
+    const predictedPoints = Array.from(xs).map((val, i) => {
+      return {x: val, y: preds[i]}
+    });
+    
+    const originalPoints = inputData.map(d => ({
+      x: d.horsepower, y: d.mpg,
+    }));
+    
+    
+    tfvis.render.scatterplot(
+      {name: 'Model Predictions vs Original Data'}, 
+      {values: [originalPoints, predictedPoints], series: ['original', 'predicted']}, 
+      {
+        xLabel: 'Horsepower',
+        yLabel: 'MPG',
+        height: 300
+      }
+    );
+  }
+
+  async function run() {
+    const data = await getData();
+    const values = data.map(d => ({
+      x: d.horsepower,
+      y: d.mpg,
+    }));
+  
+    tfvis.render.scatterplot(
+      {name: 'Horsepower v MPG'},
+      {values}, 
+      {
+        xLabel: 'Horsepower',
+        yLabel: 'MPG',
+        height: 300
+      }
+    );
+ 
+    const model=createmodel();
+    tfvis.show.modelSummary({name: 'Model Summary'}, model);
+    const tensorData=convertToTensor(data);
+
+    const {inputs,labels}=tensorData;
+    await trainmodel(model,inputs,labels);
+    console.log('done training');
+    testModel(model,data,tensorData);
+    alert('testing complete');
+  }  
+  document.addEventListener('DOMContentLoaded', run);
+  
+
+ 
+
